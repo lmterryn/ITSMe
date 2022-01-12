7b18eb95ee43b3a29543509a6701752cffc39930
lmterryn <terrynlouise@outlook.com>
2022-01-12 13:51:27 +0100

    Add functions to extract basic metrics from TreeQSMs

---
+++ R/basic_structural_metrics.R
@@ -0,0 +1,133 @@
+#' Determine the position of a treeQSM
+#'
+#' @param cylinder Cylinder field of a TreeQSM which is imported in the
+#' global environment with the read_tree_qsm function.
+#'
+#' @return The XY coordinates (location) of the tree stem.
+#' @export
+#'
+#' @examples
+#' #QSM_path <- "path/to/test_qsm.txt"
+#' #read_tree_qsm(QSM_path)
+#' #pos <- tree_position_qsm(cylinder)
+tree_position_qsm <- function(cylinder) {
+  j <- 0
+  while (sum(cylinder$length[1:(j+1)]) < 1.3)
+  {
+    j <- j+1
+  }
+
+  x_location <- cylinder$start[j+1,1]
+  y_location <- cylinder$start[j+1,2]
+  return(c(x_location,y_location))
+}
+
+#'Extract the total cylinder length from the treedata of a TreeQSM
+#'
+#' @param treedata Treedata field of a TreeQSM which is imported in the
+#' global environment with the read_tree_qsm function.
+#'
+#' @return The total length of all the cylinders (branch and trunk) in m of a TreeQSM.
+#' @export
+#'
+#' @examples
+#' #QSM_path <- "path/to/test_qsm.txt"
+#' #read_tree_qsm(QSM_path)
+#' #cyllen <- total_cyl_length_qsm(treedata)
+total_cyl_length_qsm <- function(treedata) {
+  return(treedata$TotalLength[1])
+}
+
+#' Extract the total tree volume from the treedata of a TreeQSM
+#'
+#' @param treedata Treedata field of a TreeQSM which is imported in the
+#' global environment with the read_tree_qsm function.
+#'
+#' @return The total volume in L of the TreeQSM. If the trunk was modelled with triangulation
+#' the total volume is the sum of the triangulated volume of the stem (bottom),
+#' the volume of the stem cylinder (top) and the volume of the branch cylinders.
+#' @export
+#'
+#' @examples
+#' #QSM_path <- "path/to/test_qsm.txt"
+#' #read_tree_qsm(QSM_path)
+#' #totvol <- tree_volume_qsm(treedata)
+tree_volume_qsm <- function(treedata) {
+  if (length(treedata) > 83){
+    volume <- treedata$MixTotalVolume[1]
+  } else {
+    volume <- treedata$TotalVolume[1]
+  }
+  return(volume)
+}
+
+#' Extract the total trunk volume from the treedata of a TreeQSM
+#'
+#' @param treedata Treedata field of a TreeQSM which is imported in the
+#' global environment with the read_tree_qsm function.
+#'
+#' @return The total trunk volume in L of the TreeQSM. If the trunk was modelled with triangulation
+#' the total volume is the sum of the triangulated volume of the stem (bottom) and
+#' the volume of the stem cylinder (top).
+#' @export
+#'
+#' @examples
+#' #QSM_path <- "path/to/test_qsm.txt"
+#' #read_tree_qsm(QSM_path)
+#' #trunkvol <- trunk_volume_qsm(treedata)
+trunk_volume_qsm <- function(treedata) {
+  if (length(treedata) > 83){
+    volume <- treedata$MixTrunkVolume[1]
+  } else {
+    volume <- treedata$TrunkVolume[1]
+  }
+  return(volume)
+}
+
+#' Extract the total branch volume from the treedata of a TreeQSM
+#'
+#' @param treedata Treedata field of a TreeQSM which is imported in the
+#' global environment with the read_tree_qsm function.
+#'
+#' @return The total branch volume in L of the TreeQSM.
+#' @export
+#'
+#' @examples
+#' #QSM_path <- "path/to/test_qsm.txt"
+#' #read_tree_qsm(QSM_path)
+#' #branchvol <- total_branch_volume_qsm(treedata)
+total_branch_volume_qsm  <- function(treedata) {
+  return(treedata$BranchVolume[1])
+}
+
+#' Extract the tree height from the treedata of a TreeQSM
+#'
+#' @param treedata Treedata field of a TreeQSM which is imported in the
+#' global environment with the read_tree_qsm function.
+#'
+#' @return The tree height in m of the TreeQSM.
+#' @export
+#'
+#' @examples
+#' #QSM_path <- "path/to/test_qsm.txt"
+#' #read_tree_qsm(QSM_path)
+#' #height <- tree_height_qsm(treedata)
+tree_height_qsm <- function(treedata) {
+  return(treedata$TreeHeight[1])
+}
+
+#' Extract the DBH from the treedata of a TreeQSM
+#'
+#' @param treedata Treedata field of a TreeQSM which is imported in the
+#' global environment with the read_tree_qsm function.
+#'
+#' @return The DBH in m of the TreeQSM.
+#' @export
+#'
+#' @examples
+#' #QSM_path <- "path/to/test_qsm.txt"
+#' #read_tree_qsm(QSM_path)
+#' #dbh <- DBH_qsm(treedata)
+DBH_qsm <- function(treedata) {
+  return(treedata$DBHqsm[1])
+}
--- R/read_tree_qsm.R
+++ R/read_tree_qsm.R
@@ -3,14 +3,15 @@
 #' @param path The path to the TreeQSM mat file. This file contains 1 QSM
 #' produced with version 2.4.0 of TreeQSM (https://github.com/InverseTampere/TreeQSM) in matlab.
 #'
-#' @return reads the QSM (tree) and its' components (qsm, cylinder, branch and treedata) into the global environement.
+#' @return reads the QSM (tree) and its' components (qsm, cylinder, branch, treedata and triangulation) into the global environment.
 #' @export
 #'
 #' @examples
 #' #QSM_path <- "path/to/test_qsm.txt"
-#' #read_tree_qsm <- function(QSM_path)
-read_tree_qsm <- function(path) { #input: path to QSM data; output: data from QSM
+#' #read_tree_qsm(QSM_path)
+read_tree_qsm <- function(path) {
   tree <- R.matlab::readMat(path)
+  names(tree) <- c("qsm")
   qsm <- tree$qsm
   names(qsm) <- rownames(qsm)
   cylinder <- qsm$cylinder
@@ -19,7 +20,9 @@ read_tree_qsm <- function(path) { #input: path to QSM data; output: data from QS
   names(branch) <- rownames(branch)
   treedata <- qsm$treedata
   names(treedata) <- rownames(treedata)
-  out <- list(tree,qsm,cylinder,branch,treedata)
-  list2env(stats::setNames(out, c("tree", "qsm", "cylinder", "branch", "treedata")), envir = .GlobalEnv)
+  triangulation <- qsm$triangulation
+  names(triangulation) <- rownames(triangulation)
+  out <- list(tree,qsm,cylinder,branch,treedata,triangulation)
+  list2env(stats::setNames(out, c("tree", "qsm", "cylinder", "branch", "treedata","triangulation")), envir = .GlobalEnv)
   return(print("QSM has been read"))
 }
